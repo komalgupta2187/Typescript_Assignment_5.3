29ad076fccb37ce14ab0e704400b37835e8f7922
komal Arya <guptakomal87@gmail.com>
2017-05-18 14:52:44 +0530

    Typescript_Assignment_5.3

---
+++ staticMethodDecorator.ts
@@ -0,0 +1,30 @@
+/*
+ Create a Static Method decorator, name it as MyStaticMethodDecorator and
+ define target function and display message once
+ called with class MyStaticMethodDecoratorExample.
+
+ Console following values:
+ ● target,
+ ● propertyKey,
+ ● descriptor
+ */
+
+function MyStaticMethodDecorator(
+    target: Function,
+    propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<any>
+) {
+    console.log(`MyStaticMethodDecorator called on: 
+        target function - ${target}, 
+        propertyKey - ${propertyKey},
+        descriptor - ${descriptor}`);
+}
+
+class MyStaticMethodDecoratorExample {
+    @MyStaticMethodDecorator
+    static print(output: string) {
+        console.log(`MyStaticMethodDecoratorExample.print(${output}) called.`);
+    }
+}
+
+MyStaticMethodDecoratorExample.print("test");
